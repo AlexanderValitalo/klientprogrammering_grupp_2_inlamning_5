376d4bb822a4bab9d6a2948f033c1d1433524b9b
Alexander Välitalo <143487921+AlexanderValitalo@users.noreply.github.com>
2024-04-29 10:50:40 +0200

    Make components separate
    Make components separate to make pages able to be server rendered instead of client rendered

---
+++ src/components/detailRecipe/DetailRecipe.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import openDatabase from "@/data/db";
+import { useEffect, useState } from "react";
+import styles from "./DetailRecipe.module.css";
+
+export default function DetailRecipe({ recipeData }) {
+  const [displayedRecipe, setDisplayedRecipe] = useState([]);
+
+  useEffect(() => {
+    async function doDBOperations() {
+      const db = await openDatabase();
+      const id = parseInt(recipeData.recipeId);
+      const taskFromDb = await db.get("recipes", id);
+
+      setDisplayedRecipe(taskFromDb);
+    }
+
+    doDBOperations();
+  }, []);
+
+  return (
+    <div className={styles.recipeDiv}>
+      {displayedRecipe && (
+        <>
+          <h1>{displayedRecipe.title}</h1>
+          <h2 className={styles.h2}>Ingredients</h2>
+          <p className={styles.p}>{displayedRecipe.ingredients}</p>
+          <h2 className={styles.h2}>Cooking instructions</h2>
+          <p>{displayedRecipe.cookingInstructions}</p>
+        </>
+      )}
+    </div>
+  );
+}
